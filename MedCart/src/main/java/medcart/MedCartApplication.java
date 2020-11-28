1e03ff4186958c62db2ab9244595da8acc88daec
unknown <abhibiyal@gmail.com>
2020-11-28 15:58:10 +0530

    First Commit

---
+++ MedCart/src/main/java/medcart/MedCartApplication.java
@@ -0,0 +1,13 @@
+package medcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedCartApplication.class, args);
+	}
+
+}
---
+++ MedCart/src/test/java/com/example/medhome/MedCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
